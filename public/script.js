9f83e0ce9f79621bf9a0cb3e703b1baa4de4405b
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-05-18 14:03:47 +0500

    show video on screen done

---
+++ public/script.js
@@ -0,0 +1,21 @@
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+// get media input and output access from chrome
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
