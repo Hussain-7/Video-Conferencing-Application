b511d92f46428cd06b041345230a22ef1b32bb7f
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-05-18 20:56:54 +0500

    Sending and receiving streams from connected users

--- public/script.js
+++ public/script.js
@@ -1,16 +1,48 @@
+var socket = io.connect("http://localhost:3030/");
 const videoGrid = document.getElementById("video-grid");
+
+var peer = new Peer();
+
+let myVideoStream;
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
 // get media input and output access from chrome
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
   })
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      console.log("Answering Call");
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+
+//Frontend User
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 const app = express();
 const server = require("http").Server(app);
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
 });
+const { v4: uuidV4 } = require("uuid");
 
+// configrations
+app.use("/peerjs", peerServer);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+//Backend User
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
 });
 
 server.listen(3030);
